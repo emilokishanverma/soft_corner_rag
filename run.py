214ef84f47e7cb31710f0c37bcd442b93cf65b66
Kishan Verma <your_email@example.com>
2026-04-21 18:15:27 +0530

    all_changes _made

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import uvicorn
 
 try:
     if __name__ == "__main__":
-        uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+        uvicorn.run("app.main:app", host="127.0.0.1", port=5000, reload=True)
 except Exception as e:
     raise RuntimeError(f"Failed to start server: {str(e)}")
